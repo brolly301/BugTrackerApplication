99cdf67bb53a47c126a60ab7b2ab6a498f789d59
Marc Brolly <marcrobertjohn@gmail.com>
2023-10-19 21:52:40 +0100

    Add position sticky to navbar and sidebar so they are still visible when the user scrolls

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -19,24 +19,26 @@ export default function Navbar({ currentPage }) {
   };
 
   return (
-    <div className="navbar-container">
-      <ul className="navbar-list">
-        <Link to={"/profile"} className="navbar-list-item">
-          <div className="navbar-icon-container">
-            <FaRegUserCircle className="navbar-icon" />
-          </div>
-          <li>Profile</li>
-        </Link>
+    <div className="navbar-sticky">
+      <div className="navbar-container">
+        <ul className="navbar-list">
+          <Link to={"/profile"} className="navbar-list-item">
+            <div className="navbar-icon-container">
+              <FaRegUserCircle className="navbar-icon" />
+            </div>
+            <li>Profile</li>
+          </Link>
 
-        <Link className="navbar-list-item" onClick={handleLogout}>
-          <div className="navbar-icon-container">
-            <FiLogOut className="navbar-icon" />
-          </div>
-          <li>Logout</li>
-        </Link>
-      </ul>
-      <div style={{ display: "none" }}>
-        <Outlet />
+          <Link className="navbar-list-item" onClick={handleLogout}>
+            <div className="navbar-icon-container">
+              <FiLogOut className="navbar-icon" />
+            </div>
+            <li>Logout</li>
+          </Link>
+        </ul>
+        <div style={{ display: "none" }}>
+          <Outlet />
+        </div>
       </div>
     </div>
   );
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -13,66 +13,67 @@ import { FaUsers } from "react-icons/fa";
 export default function Sidebar() {
   return (
     <div className="sidebar-container">
-      <div className="sidebar-title-container">
-        <img className="sidebar-title-icon" src={Fly} />
-        <h1 className="sidebar-title">Fly Trap</h1>
+      <div className="sidebar-sticky">
+        <div className="sidebar-title-container">
+          <img className="sidebar-title-icon" src={Fly} />
+          <h1 className="sidebar-title">Fly Trap</h1>
+        </div>
+        <hr className="sidebar-hr" />
+        <ul className="sidebar-list">
+          <Link to={"/dashboard"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <LuLayoutDashboard className="sidebar-icon" />
+            </div>
+            <li>Dashboard</li>
+          </Link>
+          <Link to={"/allTickets"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <HiOutlineTicket className="sidebar-icon" />
+            </div>
+            <li>All Tickets</li>
+          </Link>
+          <Link to={"/myTickets"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <LiaClipboardListSolid className="sidebar-icon" />
+            </div>
+            <li>My Tickets</li>
+          </Link>
+          <Link to={"/submitTicket"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <IoBugOutline className="sidebar-icon" />
+            </div>
+            <li>Submit Ticket</li>
+          </Link>
+          <Link to={"/allProjects"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <BsMotherboard className="sidebar-icon" />
+            </div>
+
+            <li>All Projects</li>
+          </Link>
+          <Link to={"/myProjects"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <VscProject className="sidebar-icon" />
+            </div>
+
+            <li>My Projects</li>
+          </Link>
+          <Link to={"/createProject"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <BsPencil className="sidebar-icon" />
+            </div>
+            <li>Create Project</li>
+          </Link>
+          <Link to={"/manageUsers"} className="sidebar-list-item-link">
+            <div className="sidebar-icon-container">
+              <FaUsers className="sidebar-icon" />
+            </div>
+            <li>Manage Users</li>
+          </Link>
+        </ul>
+
+        <hr className="sidebar-hr" />
       </div>
-      <hr className="sidebar-hr" />
-      <ul className="sidebar-list">
-        <Link to={"/dashboard"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <LuLayoutDashboard className="sidebar-icon" />
-          </div>
-          <li>Dashboard</li>
-        </Link>
-        <Link to={"/allTickets"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <HiOutlineTicket className="sidebar-icon" />
-          </div>
-          <li>All Tickets</li>
-        </Link>
-        <Link to={"/myTickets"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <LiaClipboardListSolid className="sidebar-icon" />
-          </div>
-          <li>My Tickets</li>
-        </Link>
-        <Link to={"/submitTicket"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <IoBugOutline className="sidebar-icon" />
-          </div>
-          <li>Submit Ticket</li>
-        </Link>
-        <Link to={"/allProjects"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <BsMotherboard className="sidebar-icon" />
-          </div>
-
-          <li>All Projects</li>
-        </Link>
-        <Link to={"/myProjects"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            <VscProject className="sidebar-icon" />
-          </div>
-
-          <li>My Projects</li>
-        </Link>
-        <Link to={"/createProject"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            {" "}
-            <BsPencil className="sidebar-icon" />
-          </div>
-          <li>Create Project</li>
-        </Link>
-        <Link to={"/manageUsers"} className="sidebar-list-item-link">
-          <div className="sidebar-icon-container">
-            {" "}
-            <FaUsers className="sidebar-icon" />
-          </div>
-          <li>Manage Users</li>
-        </Link>
-      </ul>
-      <hr className="sidebar-hr" />
     </div>
   );
 }
