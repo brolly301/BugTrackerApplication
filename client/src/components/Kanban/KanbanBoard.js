04283d3dbaa54df4714322cf4cad58ce0f995a82
Marc Brolly <marcrobertjohn@gmail.com>
2023-10-23 15:42:24 +0100

    Add styling for title and dropdown component for Kanban Board

--- client/src/components/Kanban/KanbanBoard.js
+++ client/src/components/Kanban/KanbanBoard.js
@@ -9,40 +9,39 @@ import useProjectContext from "../../hooks/useProjectContext";
 export default function KanbanBoard() {
   const { state, editTicket } = useTicketContext();
   const { state: projects } = useProjectContext();
-  const [project, setProject] = useState("");
-
-  console.log(project);
+  const [project, setProject] = useState("Security Audit");
 
   const labels = ["Open", "In Progress", "Testing", "Closed"];
 
   const handleEndDrag = (event) => {
     const { active, over } = event;
-
     if (active.id === over.id) {
       return;
     }
-
     editTicket({ _id: active.id, status: over.id });
   };
 
   return (
     <div className="kanban-board-container">
-      <Dropdown
-        data={project}
-        setData={setProject}
-        values={projects.map((project) => {
-          return { value: project.name, label: project.name };
-        })}
-        label={"Project"}
-      />
+      <div className="kanban-board-title-container">
+        <h2>{project}</h2>
+
+        <Dropdown
+          data={project}
+          setData={setProject}
+          values={projects.map((project) => {
+            return { value: project.name, label: project.name };
+          })}
+        />
+      </div>
+
       <DndContext onDragEnd={handleEndDrag} collisionDetection={closestCenter}>
         <div className="kanban-board-board-container">
           <KanbanColumn
             label={labels[0]}
             tickets={state.filter((ticket) => {
               return (
-                ticket.status === labels[0] &&
-                ticket.project?.name === project.project
+                ticket.status === labels[0] && ticket.project?.name === project
               );
             })}
           />
@@ -50,8 +49,7 @@ export default function KanbanBoard() {
             label={labels[1]}
             tickets={state.filter((ticket) => {
               return (
-                ticket.status === labels[1] &&
-                ticket.project?.name === project.project
+                ticket.status === labels[1] && ticket.project?.name === project
               );
             })}
           />
@@ -59,8 +57,7 @@ export default function KanbanBoard() {
             label={labels[2]}
             tickets={state.filter((ticket) => {
               return (
-                ticket.status === labels[2] &&
-                ticket.project?.name === project.project
+                ticket.status === labels[2] && ticket.project?.name === project
               );
             })}
           />
@@ -68,8 +65,7 @@ export default function KanbanBoard() {
             label={labels[3]}
             tickets={state.filter((ticket) => {
               return (
-                ticket.status === labels[3] &&
-                ticket.project?.name === project.project
+                ticket.status === labels[3] && ticket.project?.name === project
               );
             })}
           />
