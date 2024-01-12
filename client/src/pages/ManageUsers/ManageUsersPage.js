214f24261c8ce606ae4b451727fb90159c868c87
Marc Brolly <49926823+brolly301@users.noreply.github.com>
2024-01-12 15:50:00 +0000

    Add pagination to the manager users page

--- client/src/pages/ManageUsers/ManageUsersPage.js
+++ client/src/pages/ManageUsers/ManageUsersPage.js
@@ -7,11 +7,15 @@ import FilterBy from "../../components/FilterBy";
 import { manageUserFilters } from "../../functions/FilterOptions";
 import HeaderPanel from "../../components/HeaderPanel";
 import { Link } from "react-router-dom";
+import { Pagination } from "../../functions/Pagination";
+import { PiCaretLeft, PiCaretRight } from "react-icons/pi";
 
 export default function ManageUsersPage() {
   const { state } = useUserContext();
   const [search, setSearch] = useState("");
   const [filter, setFilter] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage, setUsersPerPage] = useState(10);
 
   const searchBy = state?.allUsers?.filter((user) => {
     const fullName = `${user.firstName} ${user.surname}`.toLowerCase();
@@ -20,6 +24,9 @@ export default function ManageUsersPage() {
       (fullName.match(search.toLowerCase()) && user.role === filter)
     );
   });
+
+  const { currentTickets, paginate, indexOfLastTicket, indexOfFirstTicket } =
+    Pagination(currentPage, setCurrentPage, usersPerPage, searchBy);
 
   return (
     <HeaderPanel title={"Manage Users"}>
@@ -36,7 +43,24 @@ export default function ManageUsersPage() {
           <button>Add User</button>
         </Link>
       </div>
-      <UserList state={searchBy} />
+      <UserList state={currentTickets} />
+      <div className="pagination-container">
+        <div className="pagination-previous-button">
+          {indexOfFirstTicket === 0 ? null : (
+            <PiCaretLeft onClick={() => paginate(currentPage - 1)} />
+          )}
+        </div>
+        <div className="pagination-page-number">
+          {searchBy.length < usersPerPage
+            ? null
+            : `${currentPage} of ${Math.ceil(searchBy.length / usersPerPage)}`}
+        </div>
+        <div className="pagination-next-button">
+          {indexOfLastTicket >= searchBy.length ? null : (
+            <PiCaretRight onClick={() => paginate(currentPage + 1)} />
+          )}
+        </div>
+      </div>
     </HeaderPanel>
   );
 }
